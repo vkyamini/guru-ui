d7a3080d9db7985fb1b4766a54554100b039a4ad
Yamini VK <yamini@Ashishs-MacBook-Pro.local>
2023-06-03 19:21:04 -0700

    fix Router bug

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
 import React from "react";
- import { BrowserRouter as Routes, Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginSignup from './pages/LoginSignup';
 import Profile from './pages/Profile'
 import "./styles.css";
 //import API from './utils/API'// all the fetch end-points here
 
 function App() {
-  // const [userId, setUserId] = useState(-1);
-  // const [username, setUsername] = useState("")
-  // const [token, setToken] = useState("")
-
-  // useEffect(()=>{
-  //   const storedToken = localStorage.getItem("token");
-  //   API.verifyToken(storedToken).then(data=>{
-  //     setToken(storedToken);
-  //     setUserId(data.id);
-  //     setUsername(data.username);
-  //   }).catch(err=>{
-  //     console.log(err)
-  //    logout();
-  //   })
-  // },[])
-
-
-  // const logout = ()=>{
-  //   localStorage.removeItem("token")
-  //     setToken(null);
-  //     setUsername(null);
-  //     setUserId(0);
-  // }
-
   return ( 
    <div>
-    {/* <h1>hello all</h1> */}
-    
-    <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<LoginSignup/>}/>
-    <Route path="/profile" element={<Profile/>}/>
-    </Routes>
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginSignup/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+      </Routes>
+    </Router>
    </div>
   );
    

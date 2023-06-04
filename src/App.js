b5e8fa23ba13984460ff4277ce1d17cde9ee8475
Yamini VK <yamini@Ashishs-MacBook-Pro.local>
2023-06-03 17:34:30 -0700

    added the routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+ import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 import LoginSignup from './pages/LoginSignup';
+import Profile from './pages/Profile'
 import "./styles.css";
 //import API from './utils/API'// all the fetch end-points here
 
@@ -32,7 +33,13 @@ function App() {
   return ( 
    <div>
     {/* <h1>hello all</h1> */}
-    <LoginSignup/>
+    
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<LoginSignup/>}/>
+    <Route path="/profile" element={<Profile/>}/>
+    </Routes>
+    </BrowserRouter>
    </div>
   );
    

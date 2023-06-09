5425adda9937432ea7858c8f012bba8b6a4f7bda
Yamini VK <yamini@Ashishs-MacBook-Pro.local>
2023-06-08 17:46:20 -0700

    created the Reply feature and the reply button

--- src/pages/Offers/MyOffers.js
+++ src/pages/Offers/MyOffers.js
@@ -4,9 +4,19 @@ import API from "../../api/API";
 
 export default function ShowOffers() {
   const [offers, setOffers] = useState([]);
-
+  const [replyInput, setreplyInput] = useState([]);
   const { userId } = useParams();
-
+  const handleOfferStatusUpdate = (offersId, i, status) => {
+    API.editStatusinOffers(offersId, { status })
+      .then((data) => {
+        const newOffers = [...offers]; // copy array
+        newOffers[i].status = data.data.status; // update status in offer
+        setOffers(newOffers); // update state
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   useEffect(() => {
     API.getOffersByUser(userId)
       .then((data) => {
@@ -17,23 +27,62 @@ export default function ShowOffers() {
       });
   }, [userId]);
 
+  const handleOfferMessage = (offersId, i) => {
+    API.editStatusinOffers(offersId, { Reply: replyInput })
+      .then((data) => {
+        const newOffers = [...offers];
+        newOffers[i].Reply = data.data.Reply;
+        setOffers(newOffers);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div>
       <hr></hr>
 
       <ul id="offer-ul">
         <p>My Offers</p>
-        {offers.map((offer) => (
-          <a href={`/profile/${offer.senderId._id}`}>
-            <li id="offersingle">
+
+        {offers.map((offer, i) => (
+          <li id="offersingle" key={i}>
+            <a href={`/profile/${offer.senderId._id}`}>
               <img
                 src={offer.senderId.profilepic}
                 className="tiny-profile-pic"
               />
               <span id="usersoffer">{offer.senderId.username}</span>: "
-              {offer.Text}"
-            </li>
-          </a>
+              {offer.Text}"<p>Status = {offer.status}</p>
+            </a>
+            <div>
+              <p>Reply{offer.Reply}</p>
+            </div>
+
+            <input
+              type="text"
+              value={replyInput}
+              onChange={(e) => setreplyInput(e.target.value)}
+            />
+
+            <button
+              onClick={() => handleOfferStatusUpdate(offer._id, i, "accepted")}
+              disabled={offer.status === "accepted"}
+            >
+              Approve
+            </button>
+            <button
+              onClick={() => handleOfferStatusUpdate(offer._id, i, "rejected")}
+              disabled={offer.status === "rejected"}
+            >
+              Reject
+            </button>
+            <button onClick={() => handleOfferMessage(offer._id, i)}>
+              Reply
+            </button>
+          </li>
         ))}
       </ul>
     </div>

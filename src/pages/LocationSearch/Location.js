6e00901be5f00e52f2fa4f4cbecd31eb24b12297
Yamini VK <yamini@Ashishs-MacBook-Pro.local>
2023-06-09 11:12:27 -0700

    location search

--- src/pages/LocationSearch/Location.js
+++ src/pages/LocationSearch/Location.js
@@ -1,65 +1,63 @@
-import React,{useState} from "react";
-import Navbar from "../../shared/navbar"
+import React, { useState } from "react";
+import Navbar from "../../shared/navbar";
 import API from "../../api/API";
-import PlacesDropDown from "../../shared/PlacesDropDown"
+import PlacesDropDown from "../../shared/PlacesDropDown";
 import SingleDropdown from "../../shared/SingleDropdown";
-import noProfileImg from "../Profile/no_profile.jpeg"
+import noProfileImg from "../Profile/no_profile.jpeg";
 
+export default function LocationSearch() {
+  const [locationInput, setlocationInput] = useState([]);
+  const [locationdata, setlocationdata] = useState([]);
+  const [SkillInput, setSkillInput] = useState(" ");
+  const [Fetched, setFetched] = useState(false);
+  //const [userdata , setuserdata]= ([ ])
 
-export default function LocationSearch(){
-    const [locationInput, setlocationInput] = useState([ ]);
-    const [locationdata, setlocationdata] = useState([]);
-    const [SkillInput , setSkillInput] = useState(" ")
-    const [Fetched, setFetched] = useState(false);
-    //const [userdata , setuserdata]= ([ ])
+  const searchLocation = (e) => {
+    e.preventDefault();
+    API.searchLocation(locationInput)
+      .then((data) => {
+        console.log("i am data", data);
+        setFetched(true);
+        const user = data.data;
+        console.log("i am user", user);
+        setlocationdata(user);
+      })
+      .catch((err) => {
+        console.log("error", err);
+      });
+  };
+  const LocationandSkill = (e) => {
+    e.preventDefault();
+    API.searchskillsLocation(locationInput, SkillInput)
+      .then((data) => {
+        console.log(data);
+        console.log("i am user wirth locationa and skill matching");
+        setFetched(true);
+        const user = data.data;
+        setlocationdata(user);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div>
+      <Navbar />
+      <h1>search by location</h1>
+      <div id="dropdown">
+        <PlacesDropDown setlocationInput={setlocationInput} />
+        <button onClick={searchLocation}>Search</button>
+        <SingleDropdown setSkillInput={setSkillInput} />
+        <button onClick={LocationandSkill} disabled={locationdata.length === 0}>
+          skillSearch(Teach)
+        </button>
+      </div>
 
-    const searchLocation=(e)=>{
-        e.preventDefault();
-        API.searchLocation(locationInput)
-        .then((data)=>{
-            console.log("i am data" ,data)
-            setFetched(true);
-            const user = data.data;
-            console.log("i am user",user)
-            setlocationdata(user)
-        }).catch((err)=>{
-            console.log("error",err)
-        })
-    };
-    const LocationandSkill =(e)=>{
-        e.preventDefault();
-        API.searchskillsLocation(locationInput,SkillInput)
-        .then((data)=>{
-            console.log(data);
-            console.log("i am user wirth locationa and skill matching")
-            setFetched(true);
-            const user = data.data;
-            setlocationdata(user)
-
-        }).catch((err)=>{
-            console.log(err);
-        })
-    }
-    return(
-        <div>
-            <Navbar/>
-            <h1>search by location</h1>
-            <div id="dropdown">
-           <PlacesDropDown setlocationInput={setlocationInput}/>
-           <button  onClick={searchLocation}>
-            Search
-           </button>
-           <SingleDropdown setSkillInput={setSkillInput}/>
-           <button  onClick={LocationandSkill}  disabled = {locationdata.length === 0}>
-            skillSearch
-           </button>
-            </div>
-            
-            <div id="userdiv">
+      <div id="userdiv">
         {locationdata.length === 0 && Fetched ? (
           <p id="no-matches-text">No matches found 😔</p>
         ) : (
-            locationdata.map((arr, i) => {
+          locationdata.map((arr, i) => {
             return (
               <a href={`/profile/${arr._id}`}>
                 <div id="userdisplay">
@@ -99,7 +97,6 @@ export default function LocationSearch(){
           })
         )}
       </div>
-        </div>
-    )
-
+    </div>
+  );
 }

84bcef2f935563d41699a22e546aaff17dd8674f
Yamini VK <yamini@Ashishs-MacBook-Pro.local>
2023-06-09 11:13:35 -0700

    added icons and cleared the code

--- src/pages/LoginSignup/Signup/index.js
+++ src/pages/LoginSignup/Signup/index.js
@@ -49,39 +49,54 @@ export default function SignUp() {
     <div id="container">
       <form className="login-signup-form">
         <p id="signuptext">Sign Up</p>
-        <input
-          type="text"
-          placeholder="name"
-          value={usernameInput}
-          onChange={(e) => setuserName(e.target.value)}
-          className="input-field"
-        />
-        <input
-          type="text"
-          placeholder="email@gmail.com"
-          value={emailInput}
-          onChange={(e) => setEmailInput(e.target.value)}
-          className="input-field"
-        />
-        <input
-          type="password"
-          placeholder="**************"
-          value={passwordInput}
-          onChange={(e) => setPasswordInput(e.target.value)}
-          className="input-field"
-        />
+        <diV>
+          <i class="fa fa-user icon"></i>
+          <input
+            type="text"
+            placeholder="name"
+            value={usernameInput}
+            onChange={(e) => setuserName(e.target.value)}
+            className="input-field"
+          />
+        </diV>
+        <div>
+          <i class="fa fa-envelope icon"></i>
+          <input
+            type="text"
+            placeholder="email@gmail.com"
+            value={emailInput}
+            onChange={(e) => setEmailInput(e.target.value)}
+            className="input-field"
+          />
+        </div>
+        <div>
+          <i class="fa fa-key icon"></i>
+          <input
+            type="password"
+            placeholder="**************"
+            value={passwordInput}
+            onChange={(e) => setPasswordInput(e.target.value)}
+            className="input-field"
+          />
+        </div>
+
         <div id="inputskill">
+          <i class="fa fa-plane"></i>
           <LocationDropdown setlocationInput={setlocationInput} />
         </div>
 
         <p>skills can Teach</p>
         <div id="inputskill">
+          <i class="fa fa-graduation-cap"></i>
           <Dropdown setSkillInput={setSkillInput} />
         </div>
         <p>skill you want to learn</p>
         <div id="inputskill">
+          <i class="fa fa-pencil-square-o"></i>
+          <i class="fa fa-pencil"></i>
           <UnknownSkill setskillunknown={setskillunknown} />
         </div>
+        <i class="fa fa-file-picture-o"></i>
         <button id="addpic" onClick={getProfilePicUrl}>
           Upload Profile Pic
         </button>
@@ -89,6 +104,14 @@ export default function SignUp() {
           Sign up
         </button>
       </form>
+      {/* <i class="fa fa-thumbs-down"></i>
+      <i class="fa fa-thumbs-o-up"></i>
+      <i class="fa fa-thumbs-up"></i>
+      <i class="fa fa-toggle-on"></i>
+      <i class="fa fa-trash-o"></i>
+      <i class="fa fa-user-circle-o"></i>
+      <i class="fa fa-users"></i>
+      <i class="fa fa-commenting-o"></i> */}
     </div>
   );
 }
